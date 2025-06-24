2ae9cbfde9c683959cd4cebc6cbd9ff49cc1d16c
onkarz <onkarzadbuke143@gmail.com>
2025-06-24 17:15:46 +0530

    Pushed Project Files into Git-hub Repo

---
+++ src/test/java/com/demo_mvn_app_v2/io/AppTest.java
@@ -0,0 +1,20 @@
+package com.demo_mvn_app_v2.io;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
